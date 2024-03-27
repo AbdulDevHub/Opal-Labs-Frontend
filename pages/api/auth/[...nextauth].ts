93ef0bc0e72a7ade1884c553c0c00fc1270880b5
Abdul Khan <ahkn63@gmail.com>
2024-03-27 02:53:55 -0400

    Update [...nextauth].ts

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -71,19 +71,19 @@ export default NextAuth({
 // account {
 //   provider: 'google',
 //   type: 'oauth',
-//   providerAccountId: '101335385916640648332',
-//   access_token: 'ya29.a0Ad52N3_n77OPiHVOm8umyeD5zBi3GU24Ga58jIJtCrWqAINclqIf_SngPCru06dqRgngB7b4mHdBoh3v9z6JO1YGjInCO8m3s_F50BBnieb3SZ9IMuqM9dEEgZSbm3wOKj08LP7WgqivweYwT3eaoQhMiMT-ODt9EipWaCgYKASwSARISFQHGX2MiTxCxpiPIovO8b0Qrvos9Bg0171',
-//   expires_at: 1711058707,
-//   refresh_token: '1//05AGfZrEN8tOwCgYIARAAGAUSNwF-L9Ir-hT4X-Tzp8JjwtObhWtZv1ibEdhT8pmm6nmnzw40mZ8WTK4tq_S6WnesOV-Z9K7sLe4',
+//   providerAccountId: 'number',
+//   access_token: 'string',
+//   expires_at: number,
+//   refresh_token: 'string',
 //   scope: 'openid https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile',
 //   token_type: 'Bearer',
-//   id_token: 'eyJhbGciOiJSUzI1NiIsImtpZCI6ImFkZjVlNzEwZWRmZWJlY2JlZmE5YTYxNDk1NjU0ZDAzYzBiOGVkZjgiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL2FjY291bnRzLmdvb2dsZS5jb20iLCJhenAiOiIyNDkwMTg0MzcwLTRvNXByZ2E3ZDgybTNuc2k3OGVtYTFiN3Q4MGNwMDFiLmFwcHMuZ29vZ2xldXNlcmNvbnRlbnQuY29tIiwiYXVkIjoiMjQ5MDE4NDM3MC00bzVwcmdhN2Q4Mm0zbnNpNzhlbWExYjd0ODBjcDAxYi5hcHBzLmdvb2dsZXVzZXJjb250ZW50LmNvbSIsInN1YiI6IjEwMTMzNTM4NTkxNjY0MDY0ODMzMiIsImVtYWlsIjoiYWhrbjYzQGdtYWlsLmNvbSIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJhdF9oYXNoIjoidEx0VUkxRnpsRXBUdjBJWnU0ektUQSIsIm5hbWUiOiJBYmR1bCBIYWRpIEtoYW4iLCJwaWN0dXJlIjoiaHR0cHM6Ly9saDMuZ29vZ2xldXNlcmNvbnRlbnQuY29tL2EvQUNnOG9jS0tEYURvNXVGQ0lXODA5emxmb3FQbWFPMjR5aXhUdy1DanVoOHlPRjExVEtVPXM5Ni1jIiwiZ2l2ZW5fbmFtZSI6IkFiZHVsIEhhZGkiLCJmYW1pbHlfbmFtZSI6IktoYW4iLCJpYXQiOjE3MTEwNTUxMDgsImV4cCI6MTcxMTA1ODcwOH0.vcsSuVkv08Qjn1deLefAXYuEoDf4nfHEQBbgmlK-LCfTFd-a-gjmDKccotAX9IRMhgEDtdNdzn3IVfuM-U7Hj_xgj7oGk4jeJqLJ5PyGFPmRMex-zOLeJtfeh36ta0yQftVHz57cEbDoIva9fglhwjbAPwr-KDL9hqG4C2WWx2iNONPqMXH_YN14h57Q0sFJxvawFBd1X4G1ilUZFIbwWE7IWzEoUy9J4d9szrvk4u_czwM0oPTHiMvRY_oc3MDtPVS7T-MBhgkSz9u2ESTFefQx5JiTADEO8qRj-T-PNbjfBk0qmEyRC6Ma83a_qWdnkO3KRPzx50zKPz_m58fBMw'
+//   id_token: 'string'
 // }
 
 // THIS IS AN EXAMPLE OF THE VALUE OF THE TOKEN OBJECT WHEN A USER LOGS IN
 // token {
-//   name: 'Abdul Hadi Khan',
-//   email: 'ahkn63@gmail.com',
-//   picture: 'https://lh3.googleusercontent.com/a/ACg8ocKKDaDo5uFCIW809zlfoqPmaO24yixTw-Cjuh8yOF11TKU=s96-c',
-//   sub: '101335385916640648332'
+//   name: 'string',
+//   email: 'string',
+//   picture: 'url string',
+//   sub: 'number'
 // }
