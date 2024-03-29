f70511f4e301b78d4439b4935bd899a337c4cfee
Abdul Khan <ahkn63@gmail.com>
2024-03-28 23:00:08 -0400

    Update cors.js

--- pages/api/cors.js
+++ pages/api/cors.js
@@ -7,10 +7,10 @@
  */
 export default function handler(req, res) {
   // Set headers
-  res.setHeader('Access-Control-Allow-Credentials', 'true', true)
-  res.setHeader('Access-Control-Allow-Origin', `${process.env.NEXT_PUBLIC_SITE_URL}`, true)
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE', true)
-  res.setHeader('Access-Control-Allow-Headers', 'Authorization', true)
+  res.setHeader('Access-Control-Allow-Credentials', 'true')
+  res.setHeader('Access-Control-Allow-Origin', `${process.env.NEXT_PUBLIC_SITE_URL}`)
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
+  res.setHeader('Access-Control-Allow-Headers', 'Authorization')
   // Add more headers as needed
 
   // Handle your request as you normally would
